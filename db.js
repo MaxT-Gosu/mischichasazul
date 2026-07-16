274920e120ccb457486fba004002bb672cbffffd
MaxT-Gosu <69736443+MaxT-Gosu@users.noreply.github.com>
2026-07-16 18:49:24 -0500

    Add files via upload

--- db.js
+++ db.js
@@ -3,20 +3,20 @@ const modelosDB = [
     {
         id: 1,
         nombre: "Valeria",
-        fotoPortada: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?auto=format&fit=crop&w=300&q=80",
+        fotoPortada: "https://simp1.cuckcapital.cr/images/2022-10-10_10-06.md.png",
         badge: "Hot", // Puede ser "Hot", "Nueva", o "" (vacío si no tiene)
         categoria: "Latina", // Te servirá si luego quieres poner filtros
         descripcion: "Bienvenidos a mi galería privada. Aquí verás mi contenido más exclusivo.",
         // Enlaces a sus fotos exclusivas
         fotos: [
-            "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?auto=format&fit=crop&w=600&q=80",
-            "https://images.unsplash.com/photo-1534528741775-53994a69daeb?auto=format&fit=crop&w=600&q=80",
-            "https://images.unsplash.com/photo-1517841905240-472988babdf9?auto=format&fit=crop&w=600&q=80"
+            "https://simp1.cuckcapital.cr/images/2022-10-10_10-03.md.png",
+            "https://simp1.cuckcapital.cr/images/2022-10-10_10-05_1.md.png",
+            "https://simp1.cuckcapital.cr/images/2022-10-10_10-08.md.png"
         ],
         // Enlaces a sus videos (pueden ser de servidores externos o de tu github)
         videos: [
-            "https://www.w3schools.com/html/mov_bbb.mp4", // Video de ejemplo
-            "https://www.w3schools.com/html/movie.mp4"
+            "https://www.burstcloud.co/embed/097a74af6ca1355e1ec3b0b1dd0592bc6bbd14a310bf32dae64b143243237eaa/1.mp4", // Video de ejemplo
+            "https://www.burstcloud.co/embed/9c7fa55ec5e216b50f7d58ddf2b0b7afcedead66b5cf1a0da6ddf46c054f14ef/2.mp4"
         ]
     },
     {
